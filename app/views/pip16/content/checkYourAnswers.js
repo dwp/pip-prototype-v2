497f2d3e77ec72504e10267034af3175452e5494
Gup <gup.dwp@gmail.com>
2016-01-27 15:38:48 +0000

    Created pip16 folder/routes..etc

---
+++ app/views/pip16/content/checkYourAnswers.js
@@ -0,0 +1,231 @@
+var data = [ {
+  "checkYourAnswers" : [
+    {
+      "page"               : "34",
+      "heading"            : "Your application",
+      "changeLinkTxt"      : "Answer this question",
+      "introText"          : "We need to know how your condition affects you, so you’ll be asked about:",
+      "backLink"           : "/pip16/money",
+
+      /* Helper  */
+      "helperHd"           : "Helper details",
+      "helperName"         : "Name",
+      "helperRel"          : "Relationship",
+
+      /* About You */
+      "aboutYouHd"         : "About You",
+      "aboutYouName"       : "Full name",
+      "aboutYouDOB"        : "Date of birth",
+      "aboutYouNINO"       : "National Insurance number",
+
+      /* contact Details */
+      "detaialsHD"         : "Your contact details",
+      "detailsAddress"     : "Your address",
+      "detailsCAddress"    : "Correspondence address",
+      "detailsPhone"       : "Phone number",
+      "detailsPhoneAlt"    : "Alternative phone number",
+      "detailsEmail"       : "Email address",
+
+      /* contact preferences */
+      "prefHd"             : "Contact preferences",
+      "prefSMS"            : "SMS updates",
+      "prefMobile"         : "Mobile number",
+      "prefCall"           : "Happy for us to call",
+      "prefComms"          : "Communication preferences",
+
+      /* currentWhereabouts */
+      "whereHd"            : "Your current whereabouts",
+      "whereCurrently"     : "Your currently staying in",
+      "whereDateIn"        : "Date you went in",
+      "whereAddress"       : "Address",
+
+      /* nationality */
+      "nationalityHd"      : "Nationality and where you live",
+      "nationalityCurrent" : "Current nationality",
+      "nationalityLiving"  : "Living in England, Scotland or Wales?",
+      "nationalityAbroad"  : "Have you been out of the country for more than 1 year during last 3 years?",
+
+      /* paymentsFromAbroad */
+      "paymentsHd"         : "Payments from abroad",
+      "paymentsAbroad"     : "Have you or any of your close family worked abroad or been paid benefits from outside the United Kingdom?",
+      "paymentsBenefits"   : "Have you or your close family claimed or been paid any benefits or pensions from any of these countries since your claim date?",
+      "paymentsNi"         : "Have you or your close family worked or paid national insurance in any of these countries since your claim date?",
+
+      /* conditionDetails */
+      "conditionHd"        : "Your conditions and disabilities",
+      "conditionName"      : "Name of condition",
+      "conditionWhen"      : "How long has it affected you",
+
+      /* medications */
+      "medicationHd"       : "Your medication",
+      "medicationName"     : "Name of medication",
+      "medicationDosage"   : "Dosage",
+      "medicationOften"    : "How often you take it",
+
+      /* managing medications */
+      "manageMedsHd"       : "Managing your medication",
+      "manageMedsQ"        : "Do you have difficulty managing your medication?",
+      "manageMedsWhat"     : "What aspects do you have difficulty managing?",
+      "manageMedsOften"    : "How often does your condition affect you managing your medication?",
+      "manageMedsNow"      : "How do you manage your medication now?",
+      "manageMedsDetails"  : "More details about how your condition affects you managing your medication",
+
+      /* treatments */
+      "treatmentsHd"       : "Your treatments and therapies",
+      "treatmentsHaving"   : "Are you having, or do you have planned, any treatment or therapy?",
+      "treatmentsName"     : "Name of treatment or therapy",
+      "treatmentsOften"    : "When or how often",
+      "treatmentsAtHome"   : "Do you have treatment or therapy at your home address?",
+
+      /* manageTreatments */
+      "manageTreatHd"      : "Managing your treatments",
+
+      /* side effects */
+      "sideEffectsHd"      : "Side effects",
+      "sideEffectsHave"    : "Do you get side effects from any medication, treatment or therapy?",
+      "sideEffectsGet"     : "Describe the side effects you get.",
+
+      /* condition Affects */
+      "cAffectsHd"         : "Changes to your condition",
+      "cAffectsVary"       : "Do the effects of your condition change or vary?",
+      "cAffectsHowDo"      : "How do the effects of your condition change or very?",
+      "cAffectsHowOften"   : "How often do the effects of your condition change or vary?",
+
+      /* monitoringCondition */
+      "mConditionHd"       : "Monitoring your condition",
+      "mConditionDoNeed"   : "Do you need someone to monitor your condition most days?",
+      "mConditionRoughly"  : "Roughly how many hours a week do you rely on them to help monitor your condition?",
+      "mConditionWhy"      : "Why do you rely on someone to help manage your treatments or therapy at home?",
+      "mConditionHow"      : "How does someone help you manage your treatment or therapy at home at the moment?",
+
+      /* HCP */
+      "hcpHd"              : "Professionals who know about your condition",
+      "hcpContact"         : "Do you agree to us contacting professionals who will share information about your condition with us?",
+      "hcpName"            : "Their name",
+      "hcpProfession"      : "Their profession",
+      "hcpAddress"         : "Their address",
+      "hcpNumber"          : "Their contact number",
+      "hcpLastSeen"        : "When you last saw them",
+
+      /* submitEvidence */
+      "evidenceHd"         : "Information you can send",
+      "evidenceHave"       : "Do you have medical information you’re able to send us?",
+      "evidenceWhich"      : "Which of the following are you able to send us?",
+      "evidenceHow"        : "How will you be sending us the information?",
+
+      /* speical aids */
+      "specAidsHd"         : "Aids and adaptations",
+      "specAidsWhere"      : "Where do you rely on aids, adaptations or anything else to make things easier for you?",
+      "specAidsOther"      : "List anything else you have with you all or most of the time to make things easier for you.",
+
+      /* sight */
+      "sightHd"            : "Your sight",
+      "sightDoes"          : "Does your condition affect your eyesight?",
+      "sightRoughly"       : "Roughly how often is it affected?",
+      "sightHowDoes"       : "How does your condition affect your sight?",
+
+      /* speech */
+      "speechHd"           : "Your speech",
+      "speechDoes"         : "Does your condition affect your speech?",
+      "speechRoughly"      : "Roughly how often is it affected?",
+      "speechHowDoes"      : "How does your condition affect your speech?",
+
+      /* hearing */
+      "hearingHd"          : "Your hearing",
+      "hearingDoes"        : "Does your condition affect your hearing?",
+      "hearingWhich"       : "Which of these apply?",
+      "hearingHow"         : "Explain how your condition affects your hearing",
+
+      /* getting up */
+      "gettingUpHd"        : "Getting up and moving around",
+      "gettingUpDoes"      : "Your condition affects you:",
+      "gettingUpOften"     : "How often does your condition affect you moving around your home?",
+      "gettingUpWhen"      : "When your condition affects you, how do you get up and move around your home?",
+      "gettingUpHow"       : "Explain how your condition affects you getting up and moving around.",
+
+      /* toilet */
+      "toiletHd"           : "Using the toilet",
+      "toiletDoes"         : "Does your condition affect you:",
+      "toiletOften"        : "How often does your condition affect you using the toilet?",
+      "toiletWhen"         : "When your condition affects you, how do you use the toilet?",
+      "toiletHow"          : "Explain how your condition affects you using the toilet.",
+
+      /* washing */
+      "washingHd"          : "Washing yourself",
+      "washingDoes"        : "Does your condition affect you:",
+      "washingOften"       : "How often does your condition affect you washing yourself?",
+      "washingWhen"        : "When your condition affects you, how do you wash?",
+      "washingHow"         : "Explain how your condition affects you washing",
+
+      /* gettingDressed */
+      "gettingDressedHd"    : "Getting dressed",
+      "gettingDressedDoes"  :  "Does your condition affect you:",
+      "gettingDressedOften" : "How often does your condition affect you getting dressed?",
+      "gettingDressedWhen"  : "When your condition affects you, how do you get dressed?",
+      "gettingDressedHow"   : "Explain how your condition affects you getting dressed.",
+
+      /* hotMeal */
+      "hotMealHd"           : "Making a hot meal from fresh ingredients",
+      "hotMealDoes"         : "Does your condition affect you:",
+      "hotMealOften"        : "How often does your condition affect you making a hot meal from fresh ingredients?",
+      "hotMealWhen"         : "When your condition affects you, how do you prepare and cook a hot meal from fresh ingredients?",
+      "hotMealHow"          : "Explain how your condition affects you preparing or cooking a hot meal from fresh ingredients.",
+
+      /* eatingAndDrinking */
+      "eatingAndDrinkingHd"    : "Eating and drinking",
+      "eatingAndDrinkingDoes"  : "Does your condition affect you:",
+      "eatingAndDrinkingOften" : "How often does your condition affect you eating or drinking?",
+      "eatingAndDrinkingWhen"  : "When your condition affects you, how do you eat and drink?",
+      "eatingAndDrinkingHow"   : "Explain how your condition affects you eating or drinking.",
+
+      /* gettingOut */
+      "gettingOutHd"           : "Getting out and walking about",
+      "gettingOutDoes"         : "Does your condition affect you:",
+      "gettingOutOften"        : "How often does your condition affect you getting out and walking about?",
+      "gettingOutWhen"         : "When your condition affects you, how do you get out and walking about?",
+      "gettingOutHow"          : "Explain how your condition affects you getting out or walking about.",
+
+      /* mixing */
+      "mixingHd"               : "Mixing with other people",
+      "mixingDoes"             : "Does your condition affect you:",
+      "mixingOften"            : "How often does your condition affect you mixing with people?",
+      "mixingWhen"             : "When your condition affects you, how do you mix with people?",
+      "mixingHow"              : "Explain how your condition affects you mixing with people.",
+
+      /* localJourney */
+      "localJournHd"           : "Planning a local journey",
+      "localJournWould"        : "Would your condition affect you planning a local journey to somewhere you’ve never been before?",
+      "localJournWhen"         : "How would you plan a local journey?",
+      "localJournHow"          : "Explain how your condition would affect you planning a local journey.",
+
+      /* somewhereNeverBeenBefore */
+      "somewhereNeverBeenBeforeHd"      : "Getting somewhere you have never been",
+      "somewhereNeverBeenBeforeDoes"    : "Would your condition affect you:",
+      "somewhereNeverBeenBeforeWhen"    : "How would you get to somewhere you have never been?",
+      "somewhereNeverBeenBeforeHow"     : "Explain how your condition would affect you getting to somewhere you",
+
+      /* somewhereYouKnow */
+      "somewhereYouKnowHd"              : "Getting somewhere you know",
+      "somewhereYouKnowDoes"            : "Would your condition affect you:",
+      "somewhereYouKnowWhen"            : "How would you get to somewhere you know?",
+      "somewhereYouKnowHow"             : "Explain how your condition affects you getting somewhere you know.",
+
+      /* understanding */
+      "understandingHd"                 : "Understanding written information",
+      "understandingDoes"               : "Does your condition affect your understanding different tpyes of written information?",
+      "understandingWhen"               : "How do you work out what they mean?",
+      "understandingHow"                : "Explain how your condition affects you understanding written information.",
+
+      /* money */
+      "moneyHd"                         : "Dealing with money",
+      "moneyDoes"                       : "Does your condition affect you:",
+      "moneyWhen"                       : "How do you deal with money?",
+      "moneyHow"                        : "Explain how your condition affects you dealing with money.",
+
+    },
+  ]
+}];
+
+exports.getTableData = function() {
+  return data;
+};
